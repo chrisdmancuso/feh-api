976590650e08caa8815b2c0aed02ec4ab78da0cc
chrisdmancuso <31321037+chrisdmancuso@users.noreply.github.com>
2022-07-15 18:14:49 -0400

    added endpoint for weapon, color, and move type

--- main.py
+++ main.py
@@ -44,6 +44,37 @@ game_titles = [
     "The Sacred Stones"
   ]
 
+#Helper Function
+def weapon_color_mov_helper(first='0', second='0', third='0'):
+    if first != '0':
+        first = re.sub(r'[^a-zA-Z0-9:]', ' ', first).strip()
+    if second != '0':
+        second = re.sub(r'[^a-zA-Z0-9:]', ' ', second).strip()
+    if third != '0':
+        third = re.sub(r'[^a-zA-Z0-9:]', ' ', third).strip()
+        
+    main_list = [first, second, third]
+    temp_list = main_list[:]
+    for i in main_list:
+        if i == '0':
+            temp_list.remove(i)
+    
+    results = []
+    count = len(temp_list)
+    for key, value in name_ref.items():
+        if count == 1:
+            if value[key]['Weapon Type'].lower().find(first.lower()) != -1:
+                results.append(key)
+            if value[key]['Move Type'].lower().find(first.lower()) != -1:
+                results.append(key)
+        if count == 2:
+            if (value[key]['Weapon Type'].lower().find(first.lower()) != -1 and value[key]['Weapon Type'].lower().find(second.lower()) != -1) or (value[key]['Weapon Type'].lower().find(first.lower()) != -1 and value[key]['Move Type'].lower().find(second.lower()) != -1):
+                results.append(key)
+        if count == 3:
+            if value[key]['Weapon Type'].lower().find(first.lower()) != -1 and value[key]['Weapon Type'].lower().find(second.lower()) != -1 and value[key]['Move Type'].lower().find(third.lower()) != -1:
+                results.append(key)
+    return results
+
 @app.get("/")
 async def root():
     response = RedirectResponse(url="/docs")
@@ -55,6 +86,8 @@ async def fetch_all_unit_names(unit_name: Union[str, None]=Query(default=None, d
     results_list = []
     try:
         unit_name = re.sub(r'[^a-zA-Z0-9:]', ' ', unit_name).strip()
+        unit_name = unit_name.split()
+        unit_name = ' '.join(unit_name)
     except:
         pass
     for key, value in name_ref.items():
@@ -79,6 +112,8 @@ async def fetch_all_units_detailed():
 async def fetch_units_detailed(unit_name: str=Path(default=None, description="""Retrieve detailed stats of a unit. Example, 'Abel: The Panther'"""
                                                    """ returns stats of the given unit. Run 'fetch_all_unit_names' to fetch all unit names. Is case sensitive, and must match exactly""")):
     unit_name = re.sub(r'[^a-zA-Z0-9:]', ' ', unit_name).strip()
+    unit_name = unit_name.split()
+    unit_name = ' '.join(unit_name)
     try:
         ref = fdb.reference(f"/Data/Units/{unit_name}")
     except:
@@ -97,6 +132,8 @@ async def fetch_all_units_skills():
 async def fetch_units_skills(unit_name: str=Path(default=None, description="""Retrieve detailed stats and skills of a unit. Example, 'Abel: The Panther'"""
                                                  """ returns stats and skills of the given unit. Run 'fetch_all_unit_names' to fetch all unit names. Is case sensitive, and must match exactly""")):
     unit_name = re.sub(r'[^a-zA-Z0-9:]', ' ', unit_name).strip()
+    unit_name = unit_name.split()
+    unit_name = ' '.join(unit_name)
     try:
         unit_ref = fdb.reference(f"/Data/Units/{unit_name}")
         data = unit_ref.get()
@@ -106,6 +143,27 @@ async def fetch_units_skills(unit_name: str=Path(default=None, description="""Re
     except:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND) 
     return result
+
+#Weapons
+@app.get("/api/v1/weapons-color-movement/")
+async def fetch_by_weapon_color_movement(weapon_type: str=Query(default=None, description="""Search by weapon type. 'Axe', 'Bow', 'Dagger', 'Lance', 'Sword', 'Tome', 'Staff', 'Beast', 'Breath'. """),
+                        color: str=Query(default=None, description="""Additional filter for weapon color. 'Blue', 'Colorless', 'Green', 'Red'."""),
+                        mov_type: str=Query(default=None, description="""Additional filter for movement type. 'Armored', 'Cavalry', 'Flying', 'Infantry'""")):
+    weapon_list = [weapon_type, color, mov_type]
+    temp = weapon_list[:]
+    results = []
+    for i in weapon_list:
+        if i is None:
+            temp.remove(i)
+    if len(temp) == 0:
+        return {0: 'Enter a value.'}
+    if len(temp) == 1:
+        results = weapon_color_mov_helper(temp[0])
+    if len(temp) == 2:
+        results = weapon_color_mov_helper(temp[0], temp[1])
+    if len(temp) == 3:
+        results = weapon_color_mov_helper(temp[0], temp[1], temp[2])
+    return {f"{weapon_type} {color} {mov_type}": results}
 
 #Game
 @app.get("/api/v1/games/titles")
